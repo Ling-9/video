18212da63f482cfe2c011c18ba130576607b8c18
PcLing <lingcglib@163.com>
2022-10-08 22:18:40 +0800

    整合Swagger2,新增全局统一返回结果类

---
+++ mv-beans/src/main/java/com/fgsqw/beans/common/ResultCodeEnum.java
@@ -0,0 +1,19 @@
+package com.fgsqw.beans.common;
+
+import lombok.Getter;
+
+@Getter
+public enum ResultCodeEnum {
+    SUCCESS(200,"请求成功！"),
+    FAIL(-9999,"请求失败！"),
+
+    ;
+
+    private Integer code;
+    private String message;
+
+    private ResultCodeEnum(Integer code, String message) {
+        this.code = code;
+        this.message = message;
+    }
+}
---
+++ mv-web/src/main/java/com/fgsqw/web/config/Swagger2Config.java
@@ -0,0 +1,36 @@
+package com.fgsqw.web.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+
+@Configuration
+@EnableSwagger2
+public class Swagger2Config {
+
+    @Bean
+    public Docket docket(){
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.fgsqw.web.controller"))
+                .build();
+    }
+
+    private ApiInfo apiInfo(){
+        return new ApiInfoBuilder()
+                .title("video管理文档")
+                .description("牛逼的视频网站")
+                .contact(new Contact("","",""))
+                .version("1.0")
+                .build();
+    }
+
+}
